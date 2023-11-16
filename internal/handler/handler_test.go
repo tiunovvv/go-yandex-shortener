63c3f201a8036d62c274f19459b86fe7461cfe06
tiunovvv <tiunovvv@gmail.com>
2023-11-16 19:01:29 +0100

    iter6

--- internal/handler/handler_test.go
+++ internal/handler/handler_test.go
@@ -1,154 +1,171 @@
 package handler
 
-// func TestPostHandler(t *testing.T) {
-// 	type post struct {
-// 		request string
-// 		body    string
-// 	}
-// 	type want struct {
-// 		statusCode int
-// 		body       string
-// 	}
-// 	tests := []struct {
-// 		name string
-// 		post post
-// 		want want
-// 	}{
-// 		{
-// 			name: "positive test:short body",
-// 			post: post{
-// 				request: "http://localhost:8080/",
-// 				body:    "http://www.yandex.ru",
-// 			},
-// 			want: want{
-// 				statusCode: 201,
-// 				body:       "http://localhost:8080/",
-// 			},
-// 		},
-// 		{
-// 			name: "negativ test:initial body",
-// 			post: post{
-// 				request: "http://localhost:8080/",
-// 				body:    "",
-// 			},
-// 			want: want{
-// 				statusCode: 400,
-// 				body:       "",
-// 			},
-// 		},
-// 		{
-// 			name: "negativ test:body is not url",
-// 			post: post{
-// 				request: "http://localhost:8080/",
-// 				body:    "12345",
-// 			},
-// 			want: want{
-// 				statusCode: 500,
-// 				body:       "",
-// 			},
-// 		},
-// 	}
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 
-// 	config := &config.Config{
-// 		BaseURL:       "http://localhost:8080/",
-// 		ServerAddress: "localhost:8080",
-// 	}
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/tiunovvv/go-yandex-shortener/internal/config"
+	"github.com/tiunovvv/go-yandex-shortener/internal/logger"
+	"github.com/tiunovvv/go-yandex-shortener/internal/shortener"
+	"github.com/tiunovvv/go-yandex-shortener/internal/storage"
+)
 
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			request := httptest.NewRequest(http.MethodPost, tt.post.request, bytes.NewReader([]byte(tt.post.body)))
-// 			w := httptest.NewRecorder()
+func TestPostHandler(t *testing.T) {
+	type post struct {
+		request string
+		body    string
+	}
+	type want struct {
+		statusCode int
+		body       string
+	}
+	tests := []struct {
+		name string
+		post post
+		want want
+	}{
+		{
+			name: "positive test:short body",
+			post: post{
+				request: "http://localhost:8080/",
+				body:    "http://www.yandex.ru",
+			},
+			want: want{
+				statusCode: 201,
+				body:       "http://localhost:8080/",
+			},
+		},
+		{
+			name: "negativ test:initial body",
+			post: post{
+				request: "http://localhost:8080/",
+				body:    "",
+			},
+			want: want{
+				statusCode: 400,
+				body:       "",
+			},
+		},
+		{
+			name: "negativ test:body is not url",
+			post: post{
+				request: "http://localhost:8080/",
+				body:    "12345",
+			},
+			want: want{
+				statusCode: 500,
+				body:       "",
+			},
+		},
+	}
 
-// 			storage := storage.NewStorage(config)
-// 			shortener := shortener.NewShortener(storage)
-// 			handler := NewHandler(shortener)
+	config := &config.Config{
+		BaseURL:       "http://localhost:8080/",
+		ServerAddress: "localhost:8080",
+	}
 
-// 			router := handler.InitRoutes()
-// 			router.ServeHTTP(w, request)
-// 			result := w.Result()
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			request := httptest.NewRequest(http.MethodPost, tt.post.request, bytes.NewReader([]byte(tt.post.body)))
+			w := httptest.NewRecorder()
 
-// 			assert.Equal(t, tt.want.statusCode, result.StatusCode)
+			storage := storage.NewStorage(config)
+			shortener := shortener.NewShortener(storage)
+			handler := NewHandler(shortener)
+			logger, _ := logger.InitLogger()
 
-// 			body, err := io.ReadAll(result.Body)
-// 			require.NoError(t, err)
-// 			err = result.Body.Close()
-// 			require.NoError(t, err)
-// 			assert.Contains(t, string(body), tt.want.body)
-// 		})
-// 	}
-// }
+			router := handler.InitRoutes(logger.Sugar())
+			router.ServeHTTP(w, request)
+			result := w.Result()
 
-// func TestGetHandler(t *testing.T) {
-// 	type want struct {
-// 		statusCode int
-// 		location   string
-// 	}
-// 	tests := []struct {
-// 		name     string
-// 		request  string
-// 		mapKey   string
-// 		mapValue string
-// 		want     want
-// 	}{
-// 		{
-// 			name:     "positive test #1",
-// 			mapKey:   "OWjwkttu",
-// 			mapValue: "http://www.yandex.ru",
-// 			request:  "http://localhost:8080/OWjwkttu",
-// 			want: want{
-// 				statusCode: 307,
-// 				location:   "http://www.yandex.ru",
-// 			},
-// 		},
-// 		{
-// 			name:     "negativ test: initial shortURL",
-// 			mapKey:   "OWjwkttu",
-// 			mapValue: "http://www.yandex.ru",
-// 			request:  "http://localhost:8080/",
-// 			want: want{
-// 				statusCode: 404,
-// 				location:   "",
-// 			},
-// 		},
-// 		{
-// 			name:     "negativ test: shortURL doesn't exist",
-// 			mapKey:   "OWjwkttu",
-// 			mapValue: "http://www.yandex.ru",
-// 			request:  "http://localhost:8080/123",
-// 			want: want{
-// 				statusCode: 400,
-// 				location:   "",
-// 			},
-// 		},
-// 	}
+			assert.Equal(t, tt.want.statusCode, result.StatusCode)
 
-// 	config := &config.Config{
-// 		BaseURL:       "http://localhost:8080/",
-// 		ServerAddress: "localhost:8080",
-// 	}
+			body, err := io.ReadAll(result.Body)
+			require.NoError(t, err)
+			err = result.Body.Close()
+			require.NoError(t, err)
+			assert.Contains(t, string(body), tt.want.body)
+		})
+	}
+}
 
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			request := httptest.NewRequest(http.MethodGet, tt.request, nil)
+func TestGetHandler(t *testing.T) {
+	type want struct {
+		statusCode int
+		location   string
+	}
+	tests := []struct {
+		name     string
+		request  string
+		mapKey   string
+		mapValue string
+		want     want
+	}{
+		{
+			name:     "positive test #1",
+			mapKey:   "OWjwkttu",
+			mapValue: "http://www.yandex.ru",
+			request:  "http://localhost:8080/OWjwkttu",
+			want: want{
+				statusCode: 307,
+				location:   "http://www.yandex.ru",
+			},
+		},
+		{
+			name:     "negativ test: initial shortURL",
+			mapKey:   "OWjwkttu",
+			mapValue: "http://www.yandex.ru",
+			request:  "http://localhost:8080/",
+			want: want{
+				statusCode: 404,
+				location:   "",
+			},
+		},
+		{
+			name:     "negativ test: shortURL doesn't exist",
+			mapKey:   "OWjwkttu",
+			mapValue: "http://www.yandex.ru",
+			request:  "http://localhost:8080/123",
+			want: want{
+				statusCode: 400,
+				location:   "",
+			},
+		},
+	}
 
-// 			w := httptest.NewRecorder()
-// 			storage := storage.NewStorage(config)
+	config := &config.Config{
+		BaseURL:       "http://localhost:8080/",
+		ServerAddress: "localhost:8080",
+	}
 
-// 			storage.Urls[tt.mapKey] = tt.mapValue
-// 			shortener := shortener.NewShortener(storage)
-// 			handler := NewHandler(shortener)
-// 			router := handler.InitRoutes()
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			request := httptest.NewRequest(http.MethodGet, tt.request, nil)
 
-// 			router.ServeHTTP(w, request)
-// 			result := w.Result()
+			w := httptest.NewRecorder()
+			storage := storage.NewStorage(config)
 
-// 			_, err := io.ReadAll(result.Body)
-// 			require.NoError(t, err)
-// 			err = result.Body.Close()
-// 			require.NoError(t, err)
+			storage.Urls[tt.mapKey] = tt.mapValue
+			shortener := shortener.NewShortener(storage)
+			handler := NewHandler(shortener)
+			logger, _ := logger.InitLogger()
+			router := handler.InitRoutes(logger.Sugar())
 
-// 			assert.Equal(t, tt.want.statusCode, result.StatusCode)
-// 			assert.Equal(t, tt.want.location, result.Header.Get("Location"))
-// 		})
-// 	}
-// }
+			router.ServeHTTP(w, request)
+			result := w.Result()
+
+			_, err := io.ReadAll(result.Body)
+			require.NoError(t, err)
+			err = result.Body.Close()
+			require.NoError(t, err)
+
+			assert.Equal(t, tt.want.statusCode, result.StatusCode)
+			assert.Equal(t, tt.want.location, result.Header.Get("Location"))
+		})
+	}
+}
