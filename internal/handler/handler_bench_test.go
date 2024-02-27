8ae28e2c0cd5ad2db2e91721c0c6843e864c1b97
tiunovvv <tiunovvv@gmail.com>
2024-02-27 23:38:51 +0300

    iter16

--- internal/handler/handler_bench_test.go
+++ internal/handler/handler_bench_test.go
@@ -2,154 +2,189 @@ package handler
 
 import (
 	"bytes"
-	"context"
 	"crypto/rand"
-	"fmt"
 	"math/big"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
 	"testing"
-	"time"
 
-	"github.com/gin-gonic/gin"
+	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"github.com/tiunovvv/go-yandex-shortener/internal/config"
 	"github.com/tiunovvv/go-yandex-shortener/internal/shortener"
-	"github.com/tiunovvv/go-yandex-shortener/internal/storage"
+	"github.com/tiunovvv/go-yandex-shortener/internal/storage/mocks"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
 const (
-	baseURL  = "http://localhost:8080/"
 	chars    = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 	segments = 2
 	segLen   = 5
 )
 
-func initRouter() (*gin.Engine, storage.Store, error) {
+func BenchmarkPostHandler(b *testing.B) {
 	config := config.GetConfig()
-
-	cfg := zap.NewDevelopmentConfig()
-	cfg.Level = zap.NewAtomicLevelAt(zapcore.ErrorLevel)
-
-	logger, err := cfg.Build()
-	if err != nil {
-		return nil, nil, fmt.Errorf("failed to initialize logger: %w", err)
-	}
+	logCfg := zap.NewDevelopmentConfig()
+	logCfg.Level = zap.NewAtomicLevelAt(zapcore.ErrorLevel)
+	logger, err := logCfg.Build()
+	assert.NoError(b, err)
 	log := logger.Sugar()
-
-	store := storage.NewMemory()
-	if err != nil {
-		return nil, nil, fmt.Errorf("failed to create storage: %w", err)
-	}
-
-	shortener := shortener.NewShortener(store, log)
+	ctrl := gomock.NewController(b)
+	defer ctrl.Finish()
+	m := mocks.NewMockStore(ctrl)
+	shortener := shortener.NewShortener(m, log)
 	handler := NewHandler(config, shortener, log)
 	router := handler.InitRoutes()
-	return router, store, nil
-}
-
-func BenchmarkPostHandler(b *testing.B) {
-	router, _, err := initRouter()
-	assert.Equal(b, err, nil)
-
-	url := generateRandomURL()
-
-	request := httptest.NewRequest(http.MethodPost, baseURL, bytes.NewReader([]byte(url)))
-	w := httptest.NewRecorder()
-	router.ServeHTTP(w, request)
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		request := httptest.NewRequest(http.MethodPost, baseURL, bytes.NewReader([]byte(url)))
+		b.StopTimer()
+		url := generateRandomURL()
+		m.EXPECT().SaveURL(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).Return(nil)
+		b.StartTimer()
+		request := httptest.NewRequest(http.MethodPost, "http://localhost:8080/", bytes.NewReader([]byte(url)))
 		w := httptest.NewRecorder()
 		router.ServeHTTP(w, request)
 		result := w.Result()
 		err = result.Body.Close()
-		assert.Equal(b, err, nil)
-		assert.Equal(b, http.StatusConflict, result.StatusCode)
-	}
-}
-
-func BenchmarkGetHandler(b *testing.B) {
-	router, store, err := initRouter()
-	assert.Equal(b, err, nil)
-
-	const seconds = 10 * time.Second
-	ctx, cancelCtx := context.WithTimeout(context.Background(), seconds)
-	defer cancelCtx()
-
-	urlForSave := generateRandomURL()
-	key := generateRandomSegment(segLen)
-
-	err = store.SaveURL(ctx, key, urlForSave, "")
-	assert.Equal(b, err, nil)
-
-	target, err := url.JoinPath(baseURL, key)
-	assert.Equal(b, err, nil)
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		request := httptest.NewRequest(http.MethodGet, target, nil)
-		w := httptest.NewRecorder()
-		router.ServeHTTP(w, request)
-		result := w.Result()
-		err = result.Body.Close()
-		assert.Equal(b, err, nil)
-		assert.Equal(b, http.StatusTemporaryRedirect, result.StatusCode)
-		assert.Equal(b, urlForSave, result.Header.Get("Location"))
+		assert.NoError(b, err)
+		assert.Equal(b, http.StatusCreated, result.StatusCode)
 	}
 }
 
 func BenchmarkPostAPI(b *testing.B) {
-	router, store, err := initRouter()
-	assert.Equal(b, err, nil)
-
-	const seconds = 10 * time.Second
-	ctx, cancelCtx := context.WithTimeout(context.Background(), seconds)
-	defer cancelCtx()
-	key := generateRandomSegment(segLen)
-	urlForSave := generateRandomURL()
-	body := `{"url":"` + urlForSave + `"}`
-
-	err = store.SaveURL(ctx, key, urlForSave, "")
-	assert.Equal(b, err, nil)
+	config := config.GetConfig()
+	logCfg := zap.NewDevelopmentConfig()
+	logCfg.Level = zap.NewAtomicLevelAt(zapcore.ErrorLevel)
+	logger, err := logCfg.Build()
+	assert.NoError(b, err)
+	log := logger.Sugar()
+	ctrl := gomock.NewController(b)
+	defer ctrl.Finish()
+	m := mocks.NewMockStore(ctrl)
+	shortener := shortener.NewShortener(m, log)
+	handler := NewHandler(config, shortener, log)
+	router := handler.InitRoutes()
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		urlForSave := generateRandomURL()
+		body := `{"url":"` + urlForSave + `"}`
+		m.EXPECT().SaveURL(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).Return(nil)
+		b.StartTimer()
 		request := httptest.NewRequest(http.MethodPost, "http://localhost:8080/api/shorten",
 			bytes.NewReader([]byte(body)))
 		w := httptest.NewRecorder()
 		router.ServeHTTP(w, request)
 		result := w.Result()
 		err = result.Body.Close()
-		assert.Equal(b, err, nil)
-		assert.Equal(b, http.StatusConflict, result.StatusCode)
+		assert.NoError(b, err)
+		assert.Equal(b, http.StatusCreated, result.StatusCode)
 	}
 }
 
 func BenchmarkPostAPIBatch(b *testing.B) {
-	router, _, err := initRouter()
-	assert.Equal(b, err, nil)
-
-	firstURL := generateRandomURL()
-	secondURL := generateRandomURL()
-
-	body := `[{"correlation_id": "1","original_url": "` + firstURL
-	body += `"}, {"correlation_id": "2","original_url": "` + secondURL + `"}]`
+	config := config.GetConfig()
+	logCfg := zap.NewDevelopmentConfig()
+	logCfg.Level = zap.NewAtomicLevelAt(zapcore.ErrorLevel)
+	logger, err := logCfg.Build()
+	assert.NoError(b, err)
+	log := logger.Sugar()
+	ctrl := gomock.NewController(b)
+	defer ctrl.Finish()
+	m := mocks.NewMockStore(ctrl)
+	shortener := shortener.NewShortener(m, log)
+	handler := NewHandler(config, shortener, log)
+	router := handler.InitRoutes()
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		firstURL := generateRandomURL()
+		secondURL := generateRandomURL()
+		body := `[{"correlation_id": "1","original_url": "` + firstURL
+		body += `"}, {"correlation_id": "2","original_url": "` + secondURL + `"}]`
+		m.EXPECT().SaveURLBatch(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil)
+		b.StartTimer()
 		request := httptest.NewRequest(http.MethodPost,
 			"http://localhost:8080/api/shorten/batch", bytes.NewReader([]byte(body)))
 		w := httptest.NewRecorder()
 		router.ServeHTTP(w, request)
 		result := w.Result()
 		err = result.Body.Close()
-		assert.Equal(b, err, nil)
+		assert.NoError(b, err)
 		assert.Equal(b, http.StatusCreated, result.StatusCode)
+	}
+}
+
+func BenchmarkPostAPIUserURLs(b *testing.B) {
+	config := config.GetConfig()
+	logCfg := zap.NewDevelopmentConfig()
+	logCfg.Level = zap.NewAtomicLevelAt(zapcore.ErrorLevel)
+	logger, err := logCfg.Build()
+	assert.NoError(b, err)
+	log := logger.Sugar()
+	ctrl := gomock.NewController(b)
+	defer ctrl.Finish()
+	m := mocks.NewMockStore(ctrl)
+	shortener := shortener.NewShortener(m, log)
+	handler := NewHandler(config, shortener, log)
+	router := handler.InitRoutes()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		m.EXPECT().GetURLByUserID(gomock.Any(),
+			gomock.Any()).Return(map[string]string{
+			"KCcJLOWm": "http://axIsW.NAPvB",
+			"yetoic5G": "http://wJUDw.rHNqd"})
+		b.StartTimer()
+		request := httptest.NewRequest(http.MethodGet,
+			"http://localhost:8080/api/user/urls", nil)
+
+		w := httptest.NewRecorder()
+		router.ServeHTTP(w, request)
+		result := w.Result()
+		err = result.Body.Close()
+		assert.NoError(b, err)
+		assert.Equal(b, http.StatusOK, result.StatusCode)
+	}
+}
+
+func BenchmarkGetHandler(b *testing.B) {
+	config := config.GetConfig()
+	logCfg := zap.NewDevelopmentConfig()
+	logCfg.Level = zap.NewAtomicLevelAt(zapcore.ErrorLevel)
+	logger, err := logCfg.Build()
+	assert.NoError(b, err)
+	log := logger.Sugar()
+	ctrl := gomock.NewController(b)
+	defer ctrl.Finish()
+	m := mocks.NewMockStore(ctrl)
+	shortener := shortener.NewShortener(m, log)
+	handler := NewHandler(config, shortener, log)
+	router := handler.InitRoutes()
+
+	key := generateRandomSegment(segLen)
+	target, err := url.JoinPath("http://localhost:8080/", key)
+	assert.NoError(b, err)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		url := generateRandomURL()
+		m.EXPECT().GetFullURL(gomock.Any(), gomock.Any()).Return(url, false, nil)
+		b.StartTimer()
+		request := httptest.NewRequest(http.MethodGet, target, nil)
+		w := httptest.NewRecorder()
+		router.ServeHTTP(w, request)
+		result := w.Result()
+		err = result.Body.Close()
+		assert.NoError(b, err)
+		assert.Equal(b, http.StatusTemporaryRedirect, result.StatusCode)
 	}
 }
 
